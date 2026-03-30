05aff244f1a07ab8e2fdc706d305258c66f7c5e1
kioi23 <kioijgithinji23@gmail.com>
2026-03-30 20:03:10 +0300

    added isPalindrome.js file

---
+++ isPalindrome.js
@@ -0,0 +1,10 @@
+function isPalindrome(word) {
+  const reversed = word.split('').reverse().join('');
+  return word === reversed;
+}
+
+
+console.log(isPalindrome("radar"));
+console.log(isPalindrome("level"));
+console.log(isPalindrome("hello"));
+console.log(isPalindrome("Racecar"));
